dc9ad288d5e3783050a42c1524ca14128f117380
Suresh Raju <sushraju@gmail.com>
2023-08-10 11:58:11 -0700

    edits to profile

--- coolmonk.py
+++ coolmonk.py
@@ -6,21 +6,23 @@ The summary is a brief intro. You can put raw HTML into this field.
 summary = '<p><li>Varied experience in scalable cloud infrastructure, distributed systems, product development, technology management and customer success across America, Europe and Asia. <li>Responsible for building stable and reliable data pipelines across the globe that led to increased customer adoption. <li>Executed customer success plans for large scale deployments that resulted in increased hardware revenue. <li>Mentored and led engineering teams that delivered projects for problems in performance, scale, reliability and data management.</p>'
 
 education = [
-        ['Masters in Computer Science & Applications', 'Bharathiyar University', '1996 - 1999'],
-        ['BSc in Computer Science', 'Bharathiyar University', '1993 - 1996']
+        ['Masters in Computer Science & Applications', 'Bharathiyar University'],
+        ['BSc in Computer Science', 'Bharathiyar University']
         ]
 
 interests = ['jazz','travel','tech','wine','animals']
 
 skills = [
-        ['distributed systems', '80%'],
-        ['python, perl & shell scripting', '85%'],
+        ['distributed systems in java and go', '80%'],
+        ['python, perl and shell', '85%'],
+        ['kubernetes, docker, envoy', '85%'],
+        ['security, networking', '80%'],
         ['linux, solaris', '85%'],
         ['aws, gcp, oci', '80%'],
-        ['kafka, sqs, pub-sub', '75%'],
+        ['vault, terraform', '85%'],
+        ['kafka, flink, zookeeper', '75%'],
         ['redis, solr and elk', '70%'],
-        ['chef, terraform', '75%'],
-        ['kubernetes, docker', '70%'],
+        ['akamai and cloudflare', '80%'],
         ['git, mercurial', '80%']
         ]
 
@@ -35,8 +37,13 @@ The 'Description of role' field does not get escaped by the templating engine,
 so you can put raw HTML in it if you like.
 """
 experience = [
+        ['Platform & Infrastructure',
+            'Apr 2021 - present',
+            'Mux, San Francisco',
+            '<p><li>Built and scaled the different parts of the core platform upon which the video and video analytics products are built. <li>Worked on scaling the platform to support analytics for some of the largest live streaming sporting events.</p>'
+        ],
         ['Site Reliability Engineering',
-            'Jun 2020 – present',
+            'Jun 2020 – Apr 2021',
             'Optimizely, San Francisco',
             '<p><li>performance, reliability, cdn, dns, data infrastructure and automation.</p>'
         ],
@@ -82,7 +89,7 @@ it is instantiated.
 resume_data = {
     'site_title' : 'CoolMonk',
     'name' : 'Suresh Raju',
-    'tagline' : 'Scaleable infrastructure & Site reliability',
+    'tagline' : 'Scalable infrastructure & Site reliability',
     'website' : 'https://coolmonk.org',
     'linkedin' : 'linkedin.com/in/sushraju',
     'github' : 'github.com/sushraju',
